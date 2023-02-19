771115600a45f5367bb78d81735f51c23a407a12
sphanit <thedecipher.sp@gmail.com>
2023-02-19 23:39:25 +0100

    First version: Read from xml and publish on param server

--- include/platinum_bridge/bridge.h
+++ include/platinum_bridge/bridge.h
@@ -2,59 +2,33 @@
 #define BRIDGE
 
 #include "ros/ros.h"
+#include <ros/package.h>
 #include <string>
 #include <vector>
+#include <sstream>
 #include <tinyxml.h>
+#include <map>
 
 using namespace std;
 
 // Ranges of the paramters in levels
 enum Level {LV1, LV2, LV3};
 
-// robot params structure
-struct RobotParam{
-	std::vector<float> vel_x{0,0,0};
-	std::vector<float> vel_x_back{0,0,0};
-	std::vector<float> vel_y{0,0,0};
-	std::vector<float> ang_vel{0,0,0};
-	std::vector<float> acc_x{0,0,0};
-	std::vector<float> acc_y{0,0,0};
-	std::vector<float> plan_horz{0,0,0};
-	std::vector<float> band{0,0,0};
-};
-
-// human params structure
-struct HumanParam{
-	std::vector<float> radius{0,0,0};
-	std::vector<float> vel_x{0,0,0};
-	std::vector<float> vel_x_nominal{0,0,0};
-	std::vector<float> vel_x_back{0,0,0};
-	std::vector<float> vel_y{0,0,0};
-	std::vector<float> ang_vel{0,0,0};
-	std::vector<float> fov{0,0,0};
-	std::vector<float> band{0,0,0};
-
-};
-
-// social params structure
-struct SocialNorm{
-	std::vector<float> safety{0,0,0};
-	std::vector<float> visibility{0,0,0};
-	std::vector<float> passby{0,0,0};
-	std::vector<float> invis_humans{0,0,0};
-};
+typedef map<string, vector<float>> NamedList;
+typedef map<string, Level> NamedLevel;
+typedef map<string, NamedLevel> LevelMap;
 
 // Data strtcure to include all cohan params to tune
 struct CoHANParams{
-	RobotParam RParam;
-	HumanParam HParam;
-	SocialNorm SNorm;
+	NamedList RParam;
+	NamedList HParam;
+	NamedList SNorm;
 };
 
 // Data structure to define the params in terms of levels
 struct ParamList{
 	string type;
-	std::vector<Level> params;
+	vector<Level> params;
 };
 
 // Data structure for defining context
@@ -70,7 +44,7 @@ public:
 
 	~PlatinumToCohan();
 
-	// Updates the parameters on server based on the ParamList given
+	// Updates the parameters on server based on the Context given
 	bool setParams(Context context);
 
 	// Returns the existing params formatted as ParamList
@@ -78,25 +52,27 @@ public:
 
 private:
 	// Reads the params and contexts from the xml file
-	bool readXMLData();
+	bool readParamXML();
+	bool readContextXML();
+	vector<Level> toLevels(string s, string delimiter);
+	Level toLevel(string s);
+	vector<float> toFloats(string s, string delimiter); 
 
 	// Stores the params from the xml
-	CoHANParam cohan_params_;
+	CoHANParams cohan_params_;
 
 	//Stores the contexts from the xml
-	std::vector<ParamList> contexts_;
-	std::vector<ParamList> tasks_;
-	std::vector<ParamList> humans_;
+	LevelMap contexts_;
+	LevelMap tasks_;
+	LevelMap humans_;
 
 	string map_name_;
-	string data_file_name_;
-	TiXmlDocument* doc_;
+	TiXmlDocument* param_doc_;
+	TiXmlDocument* context_doc_;
 
 	//Params list
-	std::vector<string> robot_param_names_{"max_vel_x", "max_vel_x_backwards", "max_vel_y", "max_vel_theta", "acc_lim_x",
-																				"acc_lim_y", "max_global_plan_lookahead_dist", "weight_via_point"};
-	std::vector<string> human_param_names_{"agent_radius --> check this again", "max_agent_vel_x", "nominal_agent_vel_x", "max_agent_vel_x_backwards", "max_agent_vel_y", "max_agent_vel_theta",
-																			  "fov--> how can you use this?", "weight_agent_via_point"};
+	map<string, string> robot_param_names_, human_param_names_, social_param_names_;
+								
 };
 
 #endif
--- src/bridge.cpp
+++ src/bridge.cpp
@@ -1,52 +1,264 @@
-#include <bridge.h>
+#include <platinum_bridge/bridge.h>
 #define NS "/move_base/HATebLocalPlannerROS/"
+#define ParamFile "params.xml"
+#define ContextFile "hospital.xml"
 
 PlatinumToCohan::PlatinumToCohan(){
-  ros::NodeHandle nh;
+  string param_path_ = ros::package::getPath("platinum_bridge") + "/params/hospital/" + ParamFile;
+  string context_path_ = ros::package::getPath("platinum_bridge") + "/maps/hospital/" + ContextFile;
+  map_name_ = "hospital";
+
+  // Loading the xml file
+	param_doc_ = new TiXmlDocument(param_path_);
+  
+	if(!param_doc_->LoadFile())
+		ROS_ERROR("Failed to load %s. Error : %s", param_path_.c_str(), param_doc_->ErrorDesc());
+	else
+		ROS_INFO("Params file loaded");
+
+  context_doc_ = new TiXmlDocument(context_path_);
+	
+  if(!context_doc_->LoadFile())
+		ROS_ERROR("Failed to load %s. Error : %s", context_path_.c_str(), context_doc_->ErrorDesc());
+	else
+		ROS_INFO("Contexts file loaded");
+  
+	// Check if contexts file is corresponding with map_name
+	TiXmlHandle param_docHandle(context_doc_);
+	TiXmlElement* l_map = param_docHandle.FirstChild("map_name").ToElement();
+	string map_name_read = "";
+	if(NULL != l_map->Attribute("name"))
+		map_name_read = l_map->Attribute("name");
+	if(map_name_read != map_name_)
+		ROS_ERROR("Contexts file mismatches the map_name");
+	else
+		ROS_INFO("Contexts file name corresponds to map_name");
+
+  //Initialize the param topics
+	robot_param_names_ = {{"vel_x","max_vel_x"}, {"vel_x_back","max_vel_x_backwards"}, {"vel_y","max_vel_y"}, {"ang_vel","max_vel_theta"}, {"acc_x","acc_lim_x"},
+											{"acc_y","acc_lim_y"}, {"plan_horz","max_global_plan_lookahead_dist"}, {"band","weight_via_point"}};
+
+  human_param_names_ = {{"radius","agent_radius"}, {"vel_x","max_agent_vel_x"}, {"vel_x_nominal","nominal_agent_vel_x"}, {"vel_x_back","max_agent_vel_x_backwards"}, 
+	                    {"vel_y","max_agent_vel_y"}, {"ang_vel","max_agent_vel_theta"}, {"fov","fov"}, {"band","weight_agent_via_point"}};
+
+  social_param_names_ = {{"safety","min_agent_robot_dist"}, {"visibility","visibility_cost_threshold"}, {"passby","rel_vel_cost_threshold"}, {"invis_humans","invisible_human_threshold"}};	
+
+  // Extract Information from files
+	this->readParamXML();
+	this->readContextXML();
+  
+  // Call the setParams
+  Context cnt;
+  cnt.task = "social";
+  cnt.human_type = "fragile";
+  this->setParams(cnt);
+
+  ROS_INFO("Initialized");
+
 }
 
 PlatinumToCohan::~PlatinumToCohan(){
-  delete doc_;
+  delete param_doc_;
+  delete context_doc_;
 }
 
-bool PlatinumToCohan::readXMLData(){
+bool PlatinumToCohan::readContextXML(){
+	TiXmlHandle docHandle(context_doc_);
+
+	// Extracting param ranges
+	TiXmlElement* l_param = docHandle.FirstChild("tasks").FirstChild("task").ToElement();
+
+	while(l_param){ 
+		if(NULL != l_param->Attribute("type")){
+      string t_name_ = l_param->Attribute("type");
+      TiXmlElement* l_plist = l_param->FirstChildElement("param");
+
+      while(l_plist){
+        string name_ = l_plist->Attribute("name");
+        NamedLevel tmp;
+        tmp[name_] = this->toLevel(l_plist->Attribute("level"));        
+        tasks_[t_name_] = tmp;
+        l_plist = l_plist->NextSiblingElement("param");
+      }
+    }
+		l_param = l_param->NextSiblingElement("task");
+	}
+
+  l_param = docHandle.FirstChild("humans").FirstChild("human").ToElement();
+
+	while(l_param){ 
+		if(NULL != l_param->Attribute("type")){
+      string t_name_ = l_param->Attribute("type");
+      TiXmlElement* l_plist = l_param->FirstChildElement("param");
+
+      while(l_plist){
+        string name_ = l_plist->Attribute("name");
+        NamedLevel tmp;
+        tmp[name_] = this->toLevel(l_plist->Attribute("level"));        
+        humans_[t_name_] = tmp;
+        l_plist = l_plist->NextSiblingElement("param");
+      }
+    }
+		l_param = l_param->NextSiblingElement("task");
+	}
+
+  l_param = docHandle.FirstChild("humans").FirstChild("human").ToElement();
+
+	while(l_param){ 
+		if(NULL != l_param->Attribute("type")){
+      string t_name_ = l_param->Attribute("type");
+      TiXmlElement* l_plist = l_param->FirstChildElement("param");
+
+      while(l_plist){
+        string name_ = l_plist->Attribute("name");
+        NamedLevel tmp;
+        tmp[name_] = this->toLevel(l_plist->Attribute("level"));        
+        contexts_[t_name_] = tmp;
+        l_plist = l_plist->NextSiblingElement("param");
+      }
+    }
+		l_param = l_param->NextSiblingElement("task");
+	}
 
 }
 
+bool PlatinumToCohan::readParamXML(){
+  TiXmlHandle docHandle(param_doc_);
 
+	// Extracting param ranges for robot
+	TiXmlElement* l_param = docHandle.FirstChild("params").FirstChild("r_params").FirstChild("param").ToElement();
+  
+  while(l_param){
+		if(NULL != l_param->Attribute("name")){
+      string name_ = l_param->Attribute("name");
+      string pstr = l_param->Attribute("ranges");
+      string delimiter = ", ";
+      cohan_params_.RParam[name_] = this->toFloats(pstr, delimiter);
+    }
+		l_param = l_param->NextSiblingElement("param");
+  }
 
+	l_param = docHandle.FirstChild("params").FirstChild("h_params").FirstChild("param").ToElement();
+  
+  while(l_param){
+		if(NULL != l_param->Attribute("name")){
+      string name_ = l_param->Attribute("name");
+      string pstr = l_param->Attribute("ranges");
+      string delimiter = ", ";
+      cohan_params_.HParam[name_] = this->toFloats(pstr, delimiter);
+    }
+  	l_param = l_param->NextSiblingElement("param");
+    
+  }
 
+	l_param = docHandle.FirstChild("params").FirstChild("s_norms").FirstChild("param").ToElement();
 
+  while(l_param){
+    if(NULL != l_param->Attribute("name")){
+      string name_ = l_param->Attribute("name");
+      string pstr = l_param->Attribute("ranges");
+      string delimiter = ", ";
+      cohan_params_.SNorm[name_] = this->toFloats(pstr, delimiter);
+      }
+		l_param = l_param->NextSiblingElement("param");
+  }
 
+}
 
+vector<Level> PlatinumToCohan::toLevels(string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<Level> res;
+    Level lv;
 
+    while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        if(token == "l1"){
+          lv = LV1;
+        }
+        else if(token == "l2"){
+          lv = LV2;
+        }
+        else if(token == "l3"){
+          lv = LV3;
+        }
+        res.push_back(lv);
+    }
+    token = s.substr(pos_start);
+    if(token == "l1"){
+      lv = LV1;
+    }
+    else if(token == "l2"){
+      lv = LV2;
+    }
+    else if(token == "l3"){
+      lv = LV3;
+    }
+    res.push_back(lv);
 
+    return res;
+}
 
+Level PlatinumToCohan::toLevel(string s){
+  Level lv;
 
+  if(s=="l1")
+    lv = LV1;
+  else if(s=="l2")
+    lv = LV2;
+  else if(s=="l3")
+    lv = LV3;
+   
+  return lv;
+}
 
+vector<float> PlatinumToCohan::toFloats(string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<float> res;
+    while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(stod(token));
+    }
+    res.push_back (stof(s.substr(pos_start)));
+    return res;
+}
 
-
-
-int main(int argc, char** argv)
-{
-
-  ros::init(argc, argv, "platinum_bridge");
+bool PlatinumToCohan::setParams(Context context){
   ros::NodeHandle nh;
-  std::string global_name, relative_name, default_param;
-  double vel;
 
-  if(nh.getParam("/move_base/HATebLocalPlannerROS/max_vel_x", vel))
-  {
-     std::cout << vel << '\n';
-     nh.setParam("/move_base/HATebLocalPlannerROS/max_vel_x", 0.3);
-  }
-  else{
-    std::cout << "None" << '\n';
+  int i = 0;
+  for(auto iter = cohan_params_.RParam.begin();iter!=cohan_params_.RParam.end();iter++){
+    // cout << iter->second[int(tasks_[context.task][iter->first])] << endl;
+    // cout << robot_param_names_[iter->first] << endl;
+    nh.setParam(NS+robot_param_names_[iter->first],iter->second[int(tasks_[context.task][iter->first])]);
+    i++;
   }
 
+  i = 0;
+  for(auto iter = cohan_params_.HParam.begin();iter!=cohan_params_.HParam.end();iter++){
+    nh.setParam(NS+human_param_names_[iter->first],iter->second[int(humans_[context.human_type][iter->first])]);
+    i++;
+  }
+
+  i = 0;
+  for(auto iter = cohan_params_.SNorm.begin();iter!=cohan_params_.SNorm.end();iter++){
+    nh.setParam(NS+social_param_names_[iter->first],iter->second[int(contexts_[context.task+"_"+context.human_type][iter->first])]);
+    i++;
+  }
+
+}
+
+
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "platinum_bridge");
+  
+  PlatinumToCohan pc_bridge;
 
   ros::spin();
 
   return 0;
-
 }
